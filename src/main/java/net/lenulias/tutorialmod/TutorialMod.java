700d382603b03dd372fcbdb06b533d88e225f2ad
Lenulias <lenni.ylonen@gmail.com>
2025-01-08 12:56:15 +0200

    I got a new roach.

--- src/main/java/net/lenulias/tutorialmod/TutorialMod.java
+++ src/main/java/net/lenulias/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package net.lenulias.tutorialmod;
 
+import net.lenulias.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -51,6 +52,8 @@ public class TutorialMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -63,7 +66,11 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept((ModItems.BISMUTH));
+            event.accept((ModItems.RAW_BISMUTH));
+            event.accept((ModItems.REF_BISMUTH));
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
